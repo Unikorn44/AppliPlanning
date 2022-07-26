a349a00c6426e9a37537fda029a095fbb2494369
PHDupont <phmel@orange.fr>
2022-07-26 17:34:22 +0200

    Création branche acceuil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent},
+  { path: 'acceuil', component: AcceuilComponent}
 ];
 
 
