0b64859a9bf727d1f6aeba6852eaa7bee9e5f8bc
Charlotte Marion <marion.charlotte@orange.fr>
2022-07-27 21:02:00 +0200

    [Modification] app module pour ajout composant bouton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ButtonComponent } from './molecules/atomes/button/button.component';
 import { DailyWeatherComponent } from './molecules/daily-weather/daily-weather.component';
 import { EventcardComponent } from './molecules/eventcard/eventcard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './molecules/navbar/navbar.component';
 import { SidebarComponent } from './molecules/sidebar/sidebar.component';
-import { ButtonComponent } from './button/button.component';
-
 
 @NgModule({
   declarations: [
