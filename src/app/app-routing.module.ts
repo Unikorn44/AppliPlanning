7a6e4e1f061d6290a0805c18274a6782e5db4ae0
Charlotte Marion <marion.charlotte@orange.fr>
2022-07-27 21:32:59 +0200

    Revert "[Modification] app routing pour ajout route login"
    This reverts commit 8740d644839d34a561ee8aa5b4118f656b735bd3.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
-
-// Component
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'accueil', component: AccueilComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'home', component: HomeComponent},
+  { path: 'accueil', component: AccueilComponent}
 ];
 
 
