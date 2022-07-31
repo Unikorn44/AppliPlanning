4853b277f9a8592f59333b2361210d7b55fbe0f4
Charlotte Marion <marion.charlotte@orange.fr>
2022-07-31 11:36:37 +0200

    [Modification] récupération en TS identifiant et password

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   public identifiant!: String;
+  public password!: String;
 
   constructor() { }
 
@@ -16,6 +17,7 @@ export class LoginComponent implements OnInit {
 
   public onSubmitForm(): void {
     console.log(this.identifiant);
+    console.log(this.password);
   }
 
 }
