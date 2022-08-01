066ca96b3dd58100f308c3e86a4514dbedfc0928
PHDupont <phmel@orange.fr>
2022-08-01 21:09:58 +0200

    JSON météo récupéré

--- src/app/molecules/daily-weather/daily-weather.component.ts
+++ src/app/molecules/daily-weather/daily-weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-daily-weather',
@@ -12,4 +13,23 @@ export class DailyWeatherComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public meteoWidget(){
+    console.log("it's a trap !");
+
+    let dataJSON = $.getJSON('https://www.prevision-meteo.ch/services/json/paris', function(data){
+      let today = data.fcst_day_0;
+      console.log(today);
+      //console.log(ville);
+      console.log(today.date);
+      console.log('Entre : ' + today.tmin + '°' + ' et ' + today.tmax + '°');
+      console.log(today.condition);
+      console.log(today.icon);
+    });
+
+    console.log(dataJSON);
+    
+
+
+
+  }
 }
