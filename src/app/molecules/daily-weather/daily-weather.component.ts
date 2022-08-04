290108ab4f6ba1895528596688d92aba39344de4
PHDupont <phmel@orange.fr>
2022-08-04 08:28:29 +0200

    Modif° widget pour relation back-front

--- src/app/molecules/daily-weather/daily-weather.component.ts
+++ src/app/molecules/daily-weather/daily-weather.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 
-import { ActivatedRoute, Router } from '@angular/router';
-
-
 @Component({
   selector: 'app-daily-weather',
   templateUrl: './daily-weather.component.html',
@@ -12,33 +9,17 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class DailyWeatherComponent implements OnInit {
 
   public meteoData: any;
-
+  public ville: String = "paris";
 
   ngOnInit(): void {
-    console.log("I haz test");
     this.meteoWidget();
   }
 
-
   public meteoWidget(){
-    console.log("it's a trap !");
-
-    let dataJSON = $.getJSON('https://www.prevision-meteo.ch/services/json/paris', (data) =>{
-      let today = data.fcst_day_0;
-      console.log(today);
-      console.log("paris");
-      console.log(today.date);
-      console.log('Entre : ' + today.tmin + '°' + ' et ' + today.tmax + '°');
-      console.log(today.condition);
-      console.log(today.icon);
-      this.meteoData = today;
-      console.log("I haz 1! " + this.meteoData);
+    let dataJSON = $.getJSON("https://www.prevision-meteo.ch/services/json/" + this.ville , (data) =>{
+      this.meteoData = data.fcst_day_0;
     });
-
-    console.log("I haz 2 ! " + this.meteoData);
   }
-
-
 
 
 }
