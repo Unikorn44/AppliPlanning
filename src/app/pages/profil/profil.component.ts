56622b90f26d56c37f04773bcaf60130ca3d0d09
Charlotte Marion <marion.charlotte@orange.fr>
2022-08-10 15:45:26 +0200

    [Ajout] logique formulaire pour récupérer les données entrées

--- src/app/pages/profil/profil.component.ts
+++ src/app/pages/profil/profil.component.ts
@@ -6,20 +6,33 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./profil.component.scss']
 })
 export class ProfilComponent implements OnInit {
-  display: Boolean = false;
-  hide: Boolean = true;
+  public display: Boolean = false;
+  public hide: Boolean = true;
+
+  public last_name!: string;
+  public first_name!: string;
+  public birth_date!: string;
+  public phone_number!: string;
+  public email!: string;
+  public city!: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  displayForm() {
+  public displayForm() {
     this.display = true;
     this.hide = false;
   }
 
-  onSubmitForm() {
-    console.log("hello2");
+  public onSubmitForm() {
+    console.log(this.last_name);
+    console.log(this.first_name);
+    console.log(this.birth_date);
+    console.log(this.phone_number);
+    console.log(this.email);
+    console.log(this.city);
   }
+
 }
