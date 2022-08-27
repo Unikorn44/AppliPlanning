29544909a17dfddb32c5e1490fa93a786264b60f
Charlotte Marion <marion.charlotte@orange.fr>
2022-08-27 11:32:30 +0200

    [Modification] methode Post pour envoie server

--- src/app/services/AuthService.ts
+++ src/app/services/AuthService.ts
@@ -1,16 +1,25 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import {shareReplay } from 'rxjs/operators';
 import configServer from "../../Resources/configServer.json";
 
 @Injectable()
 export class AuthService {
+
+  private headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
   constructor(private http: HttpClient) {
   }
 
   login(identifiant: string, password: string) {
-    return this.http.post(configServer.origin_server + "/api/login", { identifiant, password })
+    let body = `login=${identifiant}&password=${password}`;
+    let options = {
+      headers: this.headers,
+    }
+    return this.http
+      .post(configServer.origin_server + "/api/login", body, options)
+
     // shareReplay pour empêcher le récepteur de déclencher accidentellement plusieurs requêtes POST
-    .pipe(shareReplay());
+      .pipe(shareReplay());
   }
 }
