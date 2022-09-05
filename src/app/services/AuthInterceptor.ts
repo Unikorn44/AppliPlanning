b4948ae519c6114b4ff94bab1231bdf0c21b19d0
Charlotte Marion <marion.charlotte@orange.fr>
2022-09-05 09:08:12 +0200

    [Modification] Interceptor

--- src/app/services/AuthInterceptor.ts
+++ src/app/services/AuthInterceptor.ts
@@ -1,6 +1,8 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
+@Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem("tokenBaerer");
