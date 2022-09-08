3a4a874d7012b58cec99dd963d1872a3da16315d
Charlotte Marion <marion.charlotte@orange.fr>
2022-09-08 18:17:07 +0200

    [Ajout] requete ajout collab à user

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -25,6 +25,7 @@ export class ContactComponent implements OnInit {
 
   private tokenBaerer : any = localStorage.getItem('tokenBaerer');
   private id = localStorage.getItem('id');
+  public idUser = Number(this.id);
 
   private headers = new HttpHeaders({'Authorization': this.tokenBaerer});
 
@@ -45,12 +46,33 @@ export class ContactComponent implements OnInit {
   public getUser() {
     return this.http.get<User[]>(configServer.origin_server + "/api/user/all",  {headers: this.headers})
       .subscribe(data => {
-        this.userList = data;
+        this.userList = [];
+        console.table(data);
+
+        data.forEach(user => {
+          console.log(user);
+          let add: boolean = true;
+          this.contacts.forEach(contact => {
+            if (user.id == contact.id) {
+              add = false;
+            }
+          })
+          if (add) {
+            this.userList.push(user);
+          }
+        })
+        console.table(this.userList);
+
       })
   }
 
   public addUser() {
     console.log(this.form.value);
+
+    return this.http.put<User>(configServer.origin_server + "/api/user/" + this.id + "/add/contact/" + this.form.value,  {headers: this.headers})
+      .subscribe(data => {
+        console.log(data);
+      })
   }
 
 }
